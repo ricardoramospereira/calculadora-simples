c9d86ccca73efedd56f5d7c0483d1fc67ee3e6a4
ricardoramospereira <ricardoramospereira@gmail.com>
2022-05-20 17:41:36 -0300

    inicio

---
+++ calc.py
@@ -0,0 +1,27 @@
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {'+': add, '-': subtract, '*': multiply, '/': divide}
+
+num1 = int(input('Whats is the first number: '))
+
+for symbol in operations:
+    print(symbol)
+confirm = input('Escolha uma operação: ')
+num2 = int(input('Whats is the second number: '))
+calculation = operations[confirm]
+answer = calculation(num1, num2)
+
+print(f'{num1} {confirm} {num2} = {answer}')
