5f59beb96c19d30262a28f22b9ebf64dfbd459fb
ricardoramospereira <ricardoramospereira@gmail.com>
2022-05-21 12:11:50 -0300

    function_calculadora

--- calc.py
+++ calc.py
@@ -1,3 +1,6 @@
+from art import logo
+
+
 def add(n1, n2):
     return n1 + n2
 
@@ -13,24 +16,31 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
+
 operations = {'+': add, '-': subtract, '*': multiply, '/': divide}
 
-num1 = int(input('Qual é o primeiro número? '))
 
-for symbol in operations:
-    print(symbol)
+def calculadora():
+    print(logo)
+    num1 = float(input('Qual é o primeiro número? '))
 
-resultado = True
-while resultado:
-    confirm = input('Escolha a operação? ')
-    num2 = int(input('Qual é o próximo número? '))
-    calculation = operations[confirm]
-    answer = calculation(num1, num2)
+    for symbol in operations:
+        print(symbol)
 
-    print(f'{num1} {confirm} {num2} = {answer}')
+    resultado = True
+    while resultado:
+        confirm = input('Escolha a operação? ')
+        num2 = float(input('Qual é o próximo número? '))
+        calculation = operations[confirm]
+        answer = calculation(num1, num2)
 
-    if (input(f'Quer continuar o calculo com {answer}? [S/N] ')) in 'Ss':
-        num1 = answer
-    else:
-        resultado = False
+        print(f'{num1} {confirm} {num2} = {answer}')
 
+        if (input(f'Quer continuar o calculo com {answer}? [S/N] ')) in 'Ss':
+            num1 = answer
+        else:
+            resultado = False
+            calculadora()
+
+
+calculadora()
